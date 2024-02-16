8fc281efd4d437fa26e1735a9154d15614579a0d
Olumayoku <143851666+Olumayoku@users.noreply.github.com>
2024-02-15 21:00:04 -0800

    j

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -11,7 +11,14 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here 
+let arrowRestSum = (...num) => {
+let sum = 0;
+for ( let i = 0; i < num.length; i++){
+  let total = num[i];
+  sum += total;
+  }
+return sum;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
